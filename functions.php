f79f33f974065c0f1f1f27f76935a8f11da38c74
ruudik niitmets <digibluez@gmail.com>
2012-09-12 19:32:48 +0300

    Added comments for better documentation. used @todo for tasks

--- functions.php
+++ functions.php
@@ -4,14 +4,16 @@ if ( ! isset( $content_width ) )
 	$content_width = 900; 
 
 if ( ! function_exists( 'core68sheets_setup' ) ):
-
+//
+//Setup main theme files and actions
+//TODO maybe add custom header
+//
 function core68sheets_setup() {
 
 	require_once locate_template('/templates/comment-list.php');
 	require_once locate_template('/inc/filters.php');
 	require_once locate_template('/inc/features.php');
 	require_once locate_template('/inc/rewrite.php');
-
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'post-thumbnails' );
 	remove_action( 'wp_head', 'wlwmanifest_link' );  
@@ -30,7 +32,9 @@ function core68sheets_setup() {
 }
 endif;
 add_action( 'after_setup_theme', 'core68sheets_setup' );
-
+//
+//Push script files to browser, added to footer only
+//
 function core68sheets_scripts() {
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
@@ -40,6 +44,9 @@ function core68sheets_scripts() {
 	
 }
 add_action( 'wp_enqueue_scripts', 'core68sheets_scripts' );
+//
+//Removes recent comments widget and inline style from html
+//
 function my_remove_recent_comments_style() {global $wp_widget_factory;remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));}
 add_action('widgets_init', 'my_remove_recent_comments_style');
  ?>
