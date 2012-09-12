4b3fc039036b0f2c55409b2ac2227c0a7357beb7
ruudik niitmets <digibluez@gmail.com>
2012-09-12 19:33:20 +0300

    Cleaned up html space

--- templates/head.php
+++ templates/head.php
@@ -1,36 +1,30 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-<head profile="http://gmpg.org/xfn/11">
-
+<head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 <meta name="keywords" content="68 Core Style Sheets by Ruudik N,wordpress code and design 2012 Latest HTML5 and CSS3 Themes" />
 <meta name="description" content="digibluez,ruudik,wordpress,code,design,themes,html5,css3,templates" />
-
 <link rel="stylesheet" href="/core68sheets/style.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="/css/reset.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="/css/home.css" type="text/css" media="screen" />
-<link rel="shortcut icon" href="/img/favicon.ico">
+<link rel="shortcut icon" href="/img/favicon.ico" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 <script src="/js/mdz.js"></script>
-
 <?php wp_head(); ?>
-
 </head>
-
+    
 <body <?php body_class(); ?>>
-	
-	<header>
-		
-		<hgroup>
-			<h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
-			<h2><?php bloginfo('description'); ?></h2>
-		</hgroup>
-
-		<nav id="site-navigation" role="navigation">
-		<?php wp_nav_menu( array('container' => false,  'theme_location' => 'primary', 'menu_class' => 'nav-menu' ,'items_wrap'   => '%3$s') ); ?>
-		</nav>
-	
-	</header>
+    
+<header>
+<hgroup>
+<h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+<h2><?php bloginfo('description'); ?></h2>
+</hgroup>
+<nav id="site-navigation" role="navigation">
+<?php wp_nav_menu( array('container' => false,  'theme_location' => 'primary', 'menu_class' => 'nav-menu' ,'items_wrap'   => '%3$s') ); ?>
+</nav>
+</header>
+    
