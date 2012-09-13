c3cd390a1995b5563c7262124e2cd8db6a5f8594
ruudik niitmets <digibluez@gmail.com>
2012-09-13 03:18:35 +0300

    removed nav styles and id and div elements

--- inc/features.php
+++ inc/features.php
@@ -10,16 +10,16 @@ $nav_class = 'site-navigation paging-navigation';
 if ( is_single() )
 $nav_class = 'site-navigation post-navigation';
 ?>
-<nav role="navigation" id="<?php echo $nav_id; ?>" class="<?php echo $nav_class; ?>">
+<nav>
 <?php if ( is_single() ) : ?>
-<?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'core68sheets' ) . '</span> %title' ); ?>
-<?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'core68sheets' ) . '</span>' ); ?>
+<?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'core68sheets' ) . '</span> %title' ); ?>
+<?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'core68sheets' ) . '</span>' ); ?>
 <?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : ?>
 <?php if ( get_next_posts_link() ) : ?>
-<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'core68sheets' ) ); ?></div>
+<?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'core68sheets' ) ); ?>
 <?php endif; ?>
 <?php if ( get_previous_posts_link() ) : ?>
-<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'core68sheets' ) ); ?></div>
+<?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'core68sheets' ) ); ?>
 <?php endif; ?>
 <?php endif; ?>
 </nav>
