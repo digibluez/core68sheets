c9c97fff74fad8a7dbc85533a8646b14518fe369
ruudik niitmets <digibluez@gmail.com>
2012-09-14 04:45:12 +0300

    Changed div to li

--- templates/comment-list.php
+++ templates/comment-list.php
@@ -6,41 +6,39 @@ if ( 'div' == $args['style'] ) {
 $tag = 'div';
 $add_below = 'comment';
 } else {
-$tag = 'li';
+$tag = 'div';
 $add_below = 'div-comment';
 }
-//
-//TODO clean up html add styles,remove not needed lines
-//
 ?>
 <<?php echo $tag ?> <?php comment_class(empty( $args['has_children'] ) ? '' : 'parent') ?> id="comment-<?php comment_ID() ?>">
 <?php if ( 'div' != $args['style'] ) : ?>
 
-<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
+<ol id="div-comment-<?php comment_ID() ?>">
 
 <?php endif; ?>
-<div class="comment-author vcard">
+<li>
 <?php if ($args['avatar_size'] != 0) echo get_avatar( $comment, $args['avatar_size'] ); ?>
 <?php printf(__('<cite class="fn">%s</cite> <span class="says">says:</span>'), get_comment_author_link()) ?>
-</div>
+</li>
 
 <?php if ($comment->comment_approved == '0') : ?>
-<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.') ?></em>
-<br />
+<?php _e('Your comment is awaiting moderation.') ?>
+
 <?php endif; ?>
 
-<div class="comment-meta commentmetadata"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>">
+<li><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>">
 <?php
 printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time()) ?></a><?php edit_comment_link(__('(Edit)'),'  ','' );
 ?>
-</div>
+</li>
 
 <?php comment_text() ?>
 
-<div class="reply">
+<li class="reply">
 <?php comment_reply_link(array_merge( $args, array('add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
-</div>
+</li>
 <?php if ( 'div' != $args['style'] ) : ?>
+</ol>
 </div>
 <?php endif; ?>
 <?php  } ?>
