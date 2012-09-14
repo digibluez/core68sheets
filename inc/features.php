43642e39656b4e800a3042c53c6b5ca873761251
ruudik niitmets <digibluez@gmail.com>
2012-09-15 01:23:23 +0300

    added action/ to form

--- inc/features.php
+++ inc/features.php
@@ -78,7 +78,7 @@ function core68sheets_comment_form( $args = array(), $post_id = null ) {
 <?php echo $args['must_log_in']; ?>
 <?php do_action( 'comment_form_must_log_in_after' ); ?>
 <?php else : ?>
-<form action="<?php echo site_url( '/wp-comments-post.php' ); ?>" method="post" id="<?php echo esc_attr( $args['id_form'] ); ?>">
+<form action="<?php echo home_url( 'action/wp-comments-post.php' ); ?>" method="post" id="<?php echo esc_attr( $args['id_form'] ); ?>">
 <?php do_action( 'comment_form_top' ); ?>
 <?php if ( is_user_logged_in() ) : ?>
 <?php echo apply_filters( 'comment_form_logged_in', $args['logged_in_as'], $commenter, $user_identity ); ?>
