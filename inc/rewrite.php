f4e30e46949e487f25ec8ab59d6e3e5732024aae
ruudik niitmets <digibluez@gmail.com>
2012-09-15 01:22:14 +0300

    minor edit

--- inc/rewrite.php
+++ inc/rewrite.php
@@ -2,9 +2,9 @@
 //
 //Rewrite functions only working,when custom rules are added in .htaccess
 //
-//RewriteRule ^css/(.*) /base-v2/68files/themes/core68sheets/css/$1 [L]
-//RewriteRule ^js/(.*) /base-v2/68files/themes/core68sheets/js/$1 [L]
-//RewriteRule ^img/(.*) /base-v2/68files/themes/core68sheets/img/$1 [L]
+//RewriteRule ^css/(.*) /.../.../themes/core68sheets/css/$1 [L]
+//RewriteRule ^js/(.*) /.../.../themes/core68sheets/js/$1 [L]
+//RewriteRule ^img/(.*) /.../.../themes/core68sheets/img/$1 [L]
 //
 function change_css_js_url($content) {
     $current_path = '/68files/themes/core68sheets/';
@@ -14,4 +14,5 @@ function change_css_js_url($content) {
 }
 add_filter('bloginfo_url', 'change_css_js_url');
 add_filter('bloginfo', 'change_css_js_url');
+
 ?>
