a194617d0ffc3709f7b603eb770ffa87b95df71d
ruudik niitmets <digibluez@gmail.com>
2012-09-14 04:45:29 +0300

    changed div to h1

--- templates/comments.php
+++ templates/comments.php
@@ -1,56 +1,39 @@
-<?php	if ( post_password_required() )	return;
-//
-//Cleaned up php to fit my view style
-//TODO clean up html and add styles
-//
-?>
+<?php	if ( post_password_required() )	return;?>
 
-<div id="comments" class="comments-area">
+<section id="comments">
 
 <?php if ( have_comments() ) : ?>
-<h2 class="comments-title">
+
 <?php
-printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'core68sheets' ),
+printf( _n( '%2$s comment;', '%1$s comments', get_comments_number(), 'core68sheets' ),
 number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
 ?>
-</h2>
+
 
 <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :  ?>
-<nav role="navigation" id="comment-nav-above" class="site-navigation comment-navigation">
-<h1 class="assistive-text"><?php _e( 'Comment navigation', 'core68sheets' ); ?></h1>
-<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'core68sheets' ) ); ?></div>
-<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'core68sheets' ) ); ?></div>
+<nav role="navigation" id="comment-nav-above">
+<h1><?php _e( 'Comment navigation', 'core68sheets' ); ?></h1>
+<h1><?php previous_comments_link( __( '&larr; Older Comments', 'core68sheets' ) ); ?></h1>
+<h1><?php next_comments_link( __( 'Newer Comments &rarr;', 'core68sheets' ) ); ?></h1>
 </nav>
 <?php endif; ?>
 
-<ol class="commentlist">
+
 <?php wp_list_comments( array( 'callback' => 'core68sheets_comment' ) ); ?>
-</ol>
+
 
 <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
-<nav role="navigation" id="comment-nav-below" class="site-navigation comment-navigation">
-<h1 class="assistive-text"><?php _e( 'Comment navigation', 'core68sheets' ); ?></h1>
-<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'core68sheets' ) ); ?></div>
-<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'core68sheets' ) ); ?></div>
+<nav role="navigation" id="comment-nav-below">
+<h1><?php _e( 'Comment navigation', 'core68sheets' ); ?></h1>
+<h1><?php previous_comments_link( __( '&larr; Older Comments', 'core68sheets' ) ); ?></h1>
+<h1><?php next_comments_link( __( 'Newer Comments &rarr;', 'core68sheets' ) ); ?></h1>
 </nav>
 <?php endif;  ?>
 <?php endif; ?>
 
- <?php	if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :	?>
-<p class="nocomments"><?php _e( 'Comments are closed.', 'core68sheets' ); ?></p>
+<?php	if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :	?>
+<p><?php _e( 'Comments are closed.', 'core68sheets' ); ?></p>
 <?php endif; ?>
 
-<?php
-//
-//TODO change elements and text
-//
-comment_form(array(
-'label_submit' => _('Submit'),
-'title_reply'=>'',
-'comment_notes_before' =>'<p class="must-log-in">' .  sprintf( __( 'Please <a href="%s">log in</a> to post a comment.' ), wp_login_url( apply_filters( 'the_permalink', get_permalink( ) ) ) ) . '</p>',
-'comment_notes_after' => '',
-'comment_field' => '<p class="comment-form-comment"><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
-'logged_in_as' => '<p class="logged-in-as">' . sprintf( __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>' ), admin_url( 'profile.php' ), $user_identity, wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) ) ) . '</p>',
-));
-?>
-</div>
+<?php core68sheets_comment_form();?>
+</section>
