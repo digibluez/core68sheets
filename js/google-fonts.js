7626888953cf6caf9799c9b964dc005e65acba67
ruudik niitmets <digibluez@gmail.com>
2012-09-15 01:25:44 +0300

    minimized

--- js/google-fonts.js
+++ js/google-fonts.js
@@ -1,12 +1,1 @@
- WebFontConfig = {
-        google: { families: [ 'Merienda One' ,'Ubuntu:italic,300','Voces'] }
-      };
-      (function() {
-        var wf = document.createElement('script');
-        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-            '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-        wf.type = 'text/javascript';
-        wf.async = 'true';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(wf, s);
-      })();
+ WebFontConfig = {google: { families: [ 'Merienda One' ,'Ubuntu:italic,300','Voces'] }};(function() {var wf = document.createElement('script');wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';wf.type = 'text/javascript';wf.async = 'true';var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(wf, s);})();
